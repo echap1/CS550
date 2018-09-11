762b9fd4d361ba1d1a1a54ea149d3b6a13b977a3
Unknown <echapman22@choate.edu>
2018-09-10 21:50:52 -0400

    Added HW1 and CW1

---
+++ CW1-Command-Line-Arguments/main.py
@@ -0,0 +1,31 @@
+'''
+CS550 Classwork: Commands
+September 10, 2018
+
+@author: Ethan Chapman
+
+Sources:
+    sys.argv: https://docs.python.org/3/library/sys.html
+    
+Bonus Questions:
+    1: The Working Directory
+    2: len(sys.argv) - 1
+    3: More - Ignored, Fewer - Errors
+'''
+
+import sys
+
+def main():
+    arguments = sys.argv
+    arguments.pop(0)
+    
+    greeting = "Hello, "
+    
+    for i in range(len(arguments)):
+        greeting += arguments[i]
+        if(i == len(arguments) - 1): greeting += "."
+        else: greeting += " and "
+        
+    print(greeting)
+    
+main()
---
+++ HW1-Conversation-Program/main.py
@@ -0,0 +1,39 @@
+'''
+CS550 Homework: Conversation Program
+Due September 10, 2018
+
+@author: Ethan Chapman
+
+Sources:
+
+'''
+
+import random
+
+def main():
+    name = ask("Hi, what's your name?")
+    print("Nice to meet you, " + name + ".")
+    
+    color = ask("What's your favorite color?")
+    colorResponses = ["Cool! That's mine too!", "Oh cool, mine's ", "I prefer "]
+    colors = ["red", "green", "blue", "orange"]
+    colorResponseNum = random.randint(0, len(colorResponses) - 1)
+    colorIndex = random.randint(0, len(colors) - 1)
+    while color.lower() == colors[colorIndex]:
+        colorIndex = random.randint(0, len(colors)) 
+    if colorResponseNum == 0: print(colorResponses[colorResponseNum])
+    else: print(colorResponses[colorResponseNum] + colors[colorIndex] + ".")
+    
+    sports = ["soccer", "football", "baseball", "tennis", "hockey", "volleyball"]
+    sportPlay = random.randint(0, len(sports))
+    sportWatch = random.randint(0, len(sports))
+    while sportPlay == sportWatch: sportWatch = random.randint(0, len(sports))
+    favSport = ask("What's your favorite sport?")
+    sports += favSport
+    print("Cool, I play " + sports[sportPlay] + ", but I prefer to watch " + sports[sportWatch] + ".")
+    
+def ask(prompt):
+    print(prompt)
+    return input("> ")
+    
+main()
