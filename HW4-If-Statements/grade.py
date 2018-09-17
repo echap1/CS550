22de918ffd086cbb015fdce7b3a4db171fe93ede
Unknown <echapman22@choate.edu>
2018-09-17 11:11:34 -0400

    Updated HW4

--- HW4-If-Statements/grade.py
+++ HW4-If-Statements/grade.py
@@ -6,20 +6,6 @@ Due September 14, 2018
 """
 
 import sys
-sys.path.append("..")
 
-from libs.basicCommandLine import *
-
-
-def main():
-    grade_names = ["F", "D", "D+", "C-",  "C",  "C+", "B-", "B",  "B+", "A-", "A", "A+"]
-    grade_values = [0,  1.5, 2,    2.5,   2.85, 3.2,  3.5,  3.85, 4.2,  4.5,  4.7, 4.85]
-
-    [grade_value] = BasicCommandLine.get_arg_values([Argument(float, lambda x: 0 <= x <= 5)])
-
-    grade_index = len([x for x in grade_values if x <= grade_value]) - 1
-
-    print(grade_names[grade_index])
-
-
-main()
+grades = [["F", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+"], [0, 1.5, 2, 2.5, 2.85, 3.2, 3.5, 3.85, 4.2, 4.5, 4.7, 4.85]]
+print("Out of Range!" if not 0 <= float(sys.argv[1]) <= 5 else grades[0][len([x for x in grades[1] if x <= float(sys.argv[1])])-1])
